b626408b82b4dda61f269747a204ed8d130c182f
José Guilherme Santos Riberto <145349759+zeguilherme1@users.noreply.github.com>
2025-03-23 14:23:52 -0300

    [ADD] molde.cpp

---
+++ molde/molde.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define pb push_back
+#define _ ios::sync_with_stdio(false);cin.tie(0);
+
+const int INF = 0x3f3f3f3f;
+const int MAX = 1e6 + 10;
+const int MOD = 1e9 + 7;
+
+
+int main(){_
+
+
+
+	return 0;
+}
